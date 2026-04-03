5544b47374d7b8e60689be5c338fcdf0a7164d95
HardikD2510 <hdhm2511@gmail.com>
2026-04-03 17:50:46 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,52 @@
+const questions = [
+  {
+    image: "images/q1.png",
+    options: ["2525", "2530", "2535", "NONE"],
+    answer: 0
+  },
+  {
+    image: "images/q2.png",
+    options: ["15", "16", "4", "25"],
+    answer: 1
+  },
+  {
+    image: "images/q3.png",
+    options: ["47", "110", "119", "NOT"],
+    answer: 2
+  },
+  {
+    image: "images/q4.png",
+    options: ["6", "3", "5", "2"],
+    answer: 3
+  },
+  {
+    image: "images/q5.png",
+    options: ["756", "729", "702", "717"],
+    answer: 0
+  },
+  {
+    image: "images/q6.png",
+    options: ["-3", "3", "1", "NOT"],
+    answer: 1
+  },
+  {
+    image: "images/q7.png",
+    options: ["-6", "6", "3", "-3"],
+    answer: 2
+  },
+  {
+    image: "images/q8.png",
+    options: ["NOT", "364", "343", "322"],
+    answer: 3
+  },
+  {
+    image: "images/q9.png",
+    options: ["198", "189", "216", "201"],
+    answer: 0
+  },
+  {
+    image: "images/q10.png",
+    options: ["3939", "393", "933", "110"],
+    answer: 1
+  }
+];
